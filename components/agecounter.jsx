32c32e8b7b320088fba2493cefba4cae590fd21d
Tan Nguyen <tannguyen5659@gmail.com>
2024-04-22 16:48:43 -0700

    added /more page and route, modify nested route prob

--- components/agecounter.jsx
+++ components/agecounter.jsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 
 
 export default function Agecounter() {
-    const [age, setAge] = useState(37)
+    const [age, setAge] = useState(800)
 
     const increment = () => {
         setAge(age+1);
@@ -14,11 +14,11 @@ export default function Agecounter() {
     }
 
     const reset =() => {
-        setAge(37)
+        setAge(800)
     }
   return (
     <div className="age-container">
-        <p className="age-display">Messi is {age} years old</p>
+        <p className="age-display">Messi has scored {age} goals</p>
         <Button className="counter-button" variant="primary" onClick={increment}>Increment</Button>
         <Button className="counter-button" variant="danger" onClick={reset}>Reset</Button>
         <Button className="counter-button" variant="primary" onClick={decrement}>Decrement</Button>
